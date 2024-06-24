3b7485ae95925f3f397b32caf6d1ab6fce2f1bcb
Khun <khun.ly@bstorm.be>
2024-06-24 16:29:49 +0200

    correction exercice + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DureeComponent } from './pages/duree/duree.component';
+import { TodoListComponent } from './pages/todo-list/todo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'duree', pathMatch: 'full' },
+  { path: 'duree', component: DureeComponent },
+  { path: 'todo', component: TodoListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,54 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './components/hello/hello.component';
-import { DureeComponent } from './components/duree/duree.component';
+import { DureeComponent } from './pages/duree/duree.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TodoListComponent } from './pages/todo-list/todo-list.component';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { CardModule } from 'primeng/card';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableModule } from 'primeng/table';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MenuComponent } from './layout/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloComponent,
     DureeComponent,
+    TodoListComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     // gérer les formulaires simples
     FormsModule,
     // pas encore besoin sera vu plus tard
     ReactiveFormsModule,
+    ButtonModule,
+    InputTextModule,
+    InputSwitchModule,
+    CardModule,
+    FloatLabelModule,
+    TableModule,
+    ConfirmDialogModule,
+    ToastModule,
   ],
-  providers: [],
+  providers: [
+    ConfirmationService,
+    MessageService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
